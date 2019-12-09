f7b9352031fc294d53c05c53a270cc988ab5cc3a
legin <180448w@mymail.nyp.edu.sg>
2019-12-09 16:14:18 +0800

    streamlined SCAN algorithm codes

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from algorithm import SSTFAlgorithm_lect, SSTFAlgorithm_tut, FCFSalgorithm, CSCANAlgorithm_tut, CSCANAlgorithm_lect, LookAlgorithm, SCANAlgorithm, SCANAlgorithm_lect
 
-# SSTFAlgorithm_tut.SSTFAlgorithm_tut()
-# SSTFAlgorithm_lect.SSTFAlgorithm_lect()
-# FCFSalgorithm.FcFs()
-# LookAlgorithm.Look()
-# SCANAlgorithm.SCANAlgorithm()
-# SCANAlgorithm_lect.SCANAlgorithm()
-
+SSTFAlgorithm_tut.SSTFAlgorithm_tut()
+SSTFAlgorithm_lect.SSTFAlgorithm_lect()
+FCFSalgorithm.FcFs()
+LookAlgorithm.Look()
+SCANAlgorithm.SCANAlgorithm()
+SCANAlgorithm_lect.SCANAlgorithm()
 CSCANAlgorithm_tut.CSCANAlgorithm()
 CSCANAlgorithm_lect.CSCANAlgorithm()
