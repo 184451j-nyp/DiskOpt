8ecfdf1c3f190e4b8846ea8a877db78b0d0af5e8
legin <180448w@mymail.nyp.edu.sg>
2019-12-09 17:05:47 +0800

    added CSCAN that actually works

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from algorithm import SSTFAlgorithm, FCFSalgorithm, CSCANAlgorithm, LookAlgorithm, SCANAlgorithm
+from algorithm import SSTFAlgorithm, FCFSalgorithm, CSCANAlgorithm, LookAlgorithm, SCANAlgorithm, cscan
 
 SSTFAlgorithm.SSTFAlgorithm()
 FCFSalgorithm.FcFs()
 LookAlgorithm.Look()
 SCANAlgorithm.SCANAlgorithm()
 CSCANAlgorithm.CSCANAlgorithm()
+# cscan.CSCANAlgorithm()
